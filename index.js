e69127e6f2a258c27680c08d47d901c16709268e
bernst4315 <bernst4315@gmail.com>
2024-12-12 15:54:20 -0700

    turned user input into a number

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let userInput = prompt("Enter a number between 1 and 10");
+let userInput = Number(prompt("Enter a number between 1 and 10"));
 
-let randomNum = Math.floor((Math.random() * 10) + 1);
+let randomNum = Math.floor((Math.random() * 2) + 1);
 
 if(userInput === randomNum){
     alert("You guessed right!");
 } else {
-    alert("guess again loser")
+    alert("guess again loser");
 }
 
 alert(randomNum);
-alert(userInput);
+// alert(userInput);
